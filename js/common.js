dc4601700c8bc8fa9683c147761636802d50c91f
ljf <285079883@qq.com>
2020-06-29 12:54:19 +0800

    项目管理

---
+++ js/common.js
@@ -0,0 +1,8 @@
+function getParamFromUrl(name) {
+    var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)', 'i');
+    var r = decodeURIComponent(window.location.search).substr(1).match(reg);
+    if (r != null) {
+        return unescape(r[2]);
+    }
+    return null;
+}
